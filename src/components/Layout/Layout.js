80cd0fac4d86c9959781e2c474112034e7cea4d7
TJ Hardin <MrT246007@gmail.com>
2019-03-20 02:29:46 -0500

    Added more content to the Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const layout = (props) => (
-
+    /* We use Aux HOC (Higher Order Component), so that we can return adjacent elements. */
+    /* In this main div, we will want to have a toolbar, sidebar, and backdrop. */
+    <div>
+        Toolbar, Sidebar, Backdrop
+    </div>
+    /* In the "main" element, we want to output the componet that we will wrap with this layout. */
+    <main>
+        {props.children}
+    </main>
 );
 
 export default layout;
