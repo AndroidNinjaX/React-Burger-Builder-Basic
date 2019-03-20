e1c6a414851e8e81af9fded4d3dea1158f6c413f
TJ Hardin <MrT246007@gmail.com>
2019-03-20 02:44:33 -0500

    Added note to "Aux"

--- src/hoc/Auxiliary.js
+++ src/hoc/Auxiliary.js
@@ -1,3 +1,5 @@
+//Remember we cannot name the file "Aux" in windows. See React Notes!!!!
+
 /* This is the HOC and it is simple. Just take in "props" and then return the "children". This is so we can return adjacent elements. */
 
 const aux = (props) => props.children;
