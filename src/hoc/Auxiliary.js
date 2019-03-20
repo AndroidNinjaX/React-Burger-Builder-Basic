8591ab205097ae94a843c39cfb629ae04bcc0801
TJ Hardin <MrT246007@gmail.com>
2019-03-20 02:43:06 -0500

    Added content to Aux

---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+/* This is the HOC and it is simple. Just take in "props" and then return the "children". This is so we can return adjacent elements. */
+
+const aux = (props) => props.children;
+
+export default aux;
