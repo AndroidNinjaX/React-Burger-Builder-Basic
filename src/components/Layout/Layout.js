a588d259a43c2c809f162f62261947b641232c4a
TJ Hardin <MrT246007@gmail.com>
2019-03-20 02:48:37 -0500

    Finished the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
 
 const layout = (props) => (
     /* We use Aux HOC (Higher Order Component), so that we can return adjacent elements. */
-    /* In this main div, we will want to have a toolbar, sidebar, and backdrop. */
-    <div>
-        Toolbar, Sidebar, Backdrop
-    </div>
-    /* In the "main" element, we want to output the componet that we will wrap with this layout. */
-    <main>
-        {props.children}
-    </main>
+    <Auxiliary>
+        {/* In this main div, we will want to have a toolbar, sidebar, and backdrop. */}
+        <div>
+            Toolbar, Sidebar, Backdrop
+        </div>
+        {/* In the "main" element, we want to output the componet that we will wrap with this layout. */}
+        <main>
+            {props.children}
+        </main>
+    </Auxiliary>
 );
 
 export default layout;
