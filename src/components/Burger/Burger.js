45151828c1fdb7df6dba29becdafd61215300607
TJ Hardin <MrT246007@gmail.com>
2019-03-22 03:20:22 -0500

    Added the Burger CSS to the Burger component, and imported the BurgerIngredients component, also used it 4 times

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,1 +1,19 @@
 /*This is the component, for the burger that we are rendering to the screen*/
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
+
+/* The Burger component will use the BurgerIngredients component that we created */
+const burger = (props) => {
+    return (
+        /*We make a wrapper, what the div is, to define some width and heigth of the burger*/
+        <div className={classes.Burger}>
+            <BurgerIngredients type="bread-top" />
+            <BurgerIngredients type="bread-cheese" />
+            <BurgerIngredients type="bread-meat" />
+            <BurgerIngredients type="bread-bottom" />
+        </div>  
+    );
+};
+
+export default burger;
