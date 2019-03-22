e449b8ca0693ee3d4439043525c00df7cd5b9740
TJ Hardin <MrT246007@gmail.com>
2019-03-22 03:51:34 -0500

    Added a "state" to our BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,12 +3,23 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuider extends Component {
+    /* We make out "state" that contain our ingredients */
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return(
             /*Use the Auxiliary componet because we can return adjacent elements*/
             <Auxiliary>
                 {/*Base setup is to return 2 things. Our Burger itself, and the build controls*/}
-                <Burger />
+                {/*We pass along our ingredents from our state, into the Burger component */}
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxiliary>
         );
