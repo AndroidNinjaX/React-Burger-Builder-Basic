b3882bc524980ed6d573dcc86a75b984df40db92
TJ Hardin <MrT246007@gmail.com>
2019-04-03 01:59:08 -0500

    Added controls array

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import classes from "../BuildControls/buildControls";
 
+/*We make an array of controls, this is added for convience. So we have an array we can loop, to conviently build all the "buildControls" and render them*/
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'}
+];
+
 const buildControls = (props) => (
     <div>
 
