9bf6dbdba2f714defb6d2235b6b287045d9004bb
TJ Hardin <MrT246007@gmail.com>
2019-04-03 01:55:07 -0500

    Added BuildControls and BuildControl components and initially set them up

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from "../BuildControls/buildControls";
+
+const buildControls = (props) => (
+    <div>
+
+    </div>
+);
+
+export default buildControls;
