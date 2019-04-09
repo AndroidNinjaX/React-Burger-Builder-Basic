15bbf1070812a45587b62de82b2918e4c34385a1
TJ Hardin <MrT246007@gmail.com>
2019-04-09 01:28:03 -0500

    Added "key" to the OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,10 +4,11 @@ import Auxiliary from '../../../hoc/Auxiliary';
 const orderSummary = (props) => {
 
     /*This will be the variable to get all of our ingredients. This will be in a "object" format*/
+    /*Remember the "key", we will just use "igKey" for this one, just something unique.*/
     const ingredientsSummary = Object.keys(props.ingredients)
         .map(igKey => {
             return (
-                <li>
+                <li key={igKey}>
                     <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
                 </li>   );
         }   );
