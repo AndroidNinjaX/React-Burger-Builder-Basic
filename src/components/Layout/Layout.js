41b8b3d087993bb3ae0d6effe647b9d8527862b9
TJ Hardin <MrT246007@gmail.com>
2019-04-10 02:59:58 -0500

    Added SideDrawer component and styled it.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,14 @@ import Auxiliary from '../../hoc/Auxiliary';
 /* Import the classes form our CSS file */
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     /* We use Aux HOC (Higher Order Component), so that we can return adjacent elements. */
     <Auxiliary>
         {/* In this main div, we will want to have a toolbar, sidebar, and backdrop. */}
         <Toolbar />
+        <SideDrawer />
         {/* In the "main" element, we want to output the componet that we will wrap with this layout. */}
         <main className={classes.Content}>
             {props.children}
