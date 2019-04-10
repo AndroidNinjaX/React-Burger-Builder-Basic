d680194c276fd2b6398afc1a30b03908f02ffa0e
TJ Hardin <MrT246007@gmail.com>
2019-04-10 01:48:38 -0500

    import the burger logo into the logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import burgerLogo from '../../assets/images/burger-Logo.png'
 
-const logo = (props) => (); 
+const logo = (props) => (
+    <div>
+        <img src={burgerLogo} alt="myBurger" />
+    </div>
+); 
 
 export default logo;
