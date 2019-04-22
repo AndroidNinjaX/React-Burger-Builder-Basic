9637f1884d11c1c7e9a09aa02c4a46c5c3e11542
TJ Hardin <MrT246007@gmail.com>
2019-04-22 03:24:32 -0500

    Changed OrderSummary to a class based component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+    render () {
 
-    /*This will be the variable to get all of our ingredients. This will be in a "object" format*/
-    /*Remember the "key", we will just use "igKey" for this one, just something unique.*/
-    const ingredientsSummary = Object.keys(props.ingredients)
+        /*This will be the variable to get all of our ingredients. This will be in a "object" format*/
+        /*Remember the "key", we will just use "igKey" for this one, just something unique.*/
+        const ingredientsSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>   );
         }   );
 
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            {/*This will be the list of the ingredients the user has added*/}
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            {/*Adding the price is pretty simple. Just pass the price in from "BurgerBuilder" and put it where we need to. Also use the "toFixed" to adjust the decimal places.*/}
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Auxiliary>
-    );
+        return (
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                {/*This will be the list of the ingredients the user has added*/}
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                {/*Adding the price is pretty simple. Just pass the price in from "BurgerBuilder" and put it where we need to. Also use the "toFixed" to adjust the decimal places.*/}
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Auxiliary>
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
