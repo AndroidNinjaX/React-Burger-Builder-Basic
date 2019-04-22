ec2bf5d7ac7ed4c7880ff6f4fafd67a6469e4746
TJ Hardin <MrT246007@gmail.com>
2019-04-22 02:00:26 -0500

    Changed Layout to a class based component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 /* Import the classes form our CSS file */
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    /* We use Aux HOC (Higher Order Component), so that we can return adjacent elements. */
-    <Auxiliary>
-        {/* In this main div, we will want to have a toolbar, sidebar, and backdrop. */}
-        <Toolbar />
-        <SideDrawer />
-        {/* In the "main" element, we want to output the componet that we will wrap with this layout. */}
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
-);
+class Layout extends Component {
+    render() {
+        return (
+            /* We use Aux HOC (Higher Order Component), so that we can return adjacent elements. */
+            <Auxiliary>
+                {/* In this main div, we will want to have a toolbar, sidebar, and backdrop. */}
+                <Toolbar />
+                <SideDrawer />
+                {/* In the "main" element, we want to output the componet that we will wrap with this layout. */}
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        )
+    }
+};
 
-export default layout;
+export default Layout;
