c6774c083a12525bf51d4db3a8474436c2897525
TJ Hardin <MrT246007@gmail.com>
2019-04-22 02:03:12 -0500

    added "state" and handler to Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,13 +6,21 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    
     render() {
         return (
             /* We use Aux HOC (Higher Order Component), so that we can return adjacent elements. */
             <Auxiliary>
                 {/* In this main div, we will want to have a toolbar, sidebar, and backdrop. */}
                 <Toolbar />
-                <SideDrawer />
+                <SideDrawer closed={this.sideDrawerClosedHandler}/>
                 {/* In the "main" element, we want to output the componet that we will wrap with this layout. */}
                 <main className={classes.Content}>
                     {this.props.children}
