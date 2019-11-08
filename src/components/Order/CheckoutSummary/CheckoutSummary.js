c52a5b1f9c01a86d2b7a98acb839663b59c1cb1b
TJ Hardin <MrT246007@gmail.com>
2019-11-08 01:23:15 -0600

    Add heading, burger, and buttons to CheckoutSummary component\

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,33 @@
+/* The goal of this component is to display a preview of out burger and show the "continue" or "cancel" buttons */
+import React from 'react';
+
+//Import our burger into the component.
+import Burger from '../../Burger/Burger';
+
+//Import out buttons in for "cancel" and "continue".
+import Button from '../../UI/Button/Button';
+
+const checkoutSummary = (props) => {
+    return(
+        <div>
+            {/*Heading*/}
+            <h1>We hope it taste well!</h1>
+
+            {/*We are going to display the burger here, from the burger component. We also put it in a 300x300 box.*/}
+            <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+                {/*We have to make sure to pass ingredents into the burger, which is expects as a "prop".*/}
+                <Burger ingredients={props.ingredients}/>
+            </div>
+
+            {/*Add out buttons to the Checkout Summary*/}
+            <Button 
+                btnType="Danger"
+                clicked="">CANCEL</Button>
+            <Button 
+                btnType="Success"
+                clicked="">Continue</Button>
+        </div>
+    );
+}
+
+export default checkoutSummary;
