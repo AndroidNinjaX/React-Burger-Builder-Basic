be28593ac8fe9b9635cfb1e5aed07ba4ad541912
TJ Hardin <MrT246007@gmail.com>
2019-11-09 01:41:37 -0600

    Added styling to the Contact Data

--- src/containers/Checkout/Contact Data/ContactData.js
+++ src/containers/Checkout/Contact Data/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.module.css';
 
 class ContactData extends Component {
     state = {
@@ -14,7 +15,7 @@ class ContactData extends Component {
 
     render () {
         return(
-            <div>
+            <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
                 <form>
                     <input type='text' name='name' placeholder ='Your Name' />
