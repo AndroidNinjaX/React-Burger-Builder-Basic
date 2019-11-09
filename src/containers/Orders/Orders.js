34ea52c4f541551217880a9942521df293538eab
TJ Hardin <MrT246007@gmail.com>
2019-11-09 03:31:29 -0600

    Set up orders to display 2 dummy orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
-class Order extends Component {
+import Order from '../../components/Order/Order';
+
+class Orders extends Component {
     render () {
-        return ();
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+        );
     }
 }
 
-export default Order;
+export default Orders;
