ab5f21fb35f0c6c811f4325b72fa0020e06c87d2
TJ Hardin <MrT246007@gmail.com>
2019-11-10 00:43:51 -0600

    Reach to the database to get orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+    state = {
+        order: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                console.log(res.date);
+                this.setState({loading: false});
+            })
+            .catch(err => {
+                this.setState({loading: false});
+            });
+    }
+
     render () {
         return (
             <div>
